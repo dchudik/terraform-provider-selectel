a30e9306c5ea003bf93c6ee95700fcde2d3a8d18
Dmitrii Chudinov <chudinov.d@selectel.com>
2023-12-08 16:58:19 +0300

    Concrete definition error if project_id not setup

--- selectel/domains_v2.go
+++ selectel/domains_v2.go
@@ -2,21 +2,24 @@ package selectel
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
 
 	domainsV2 "github.com/selectel/domains-go/pkg/v2"
 )
 
+var ErrProjectIDNotSetupForDNSV2 = errors.New("env variable SEL_PROJECT_ID or variable project_id must be set for the dns v2")
+
 func getDomainsV2Client(meta interface{}) (domainsV2.DNSClient[domainsV2.Zone, domainsV2.RRSet], error) {
 	config := meta.(*Config)
 	if config.ProjectID == "" {
-		return nil, fmt.Errorf("SEL_PROJECT_ID must be set for the dns v2")
+		return nil, ErrProjectIDNotSetupForDNSV2
 	}
 
 	selvpcClient, err := config.GetSelVPCClientWithProjectScope(config.ProjectID)
 	if err != nil {
-		return nil, fmt.Errorf("can't get selvpc client for domains: %w", err)
+		return nil, fmt.Errorf("can't get selvpc client for domains v2: %w", err)
 	}
 
 	httpClient := &http.Client{}
